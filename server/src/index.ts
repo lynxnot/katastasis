01350ec2cfdc907afc3327e34c0c6ed201dfae50
lynxnot <dfanciola@gmail.com>
2023-05-29 11:45:39 +0200

    introduce port const

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,12 @@
 import express, { Express, Request, Response } from 'express';
 
 const app: Express = express();
+const port: Number = 4269;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('katastasis is running');
 });
 
-app.listen(4269, () => {
-  console.log('Server is running on port 4269');
+app.listen(port, () => {
+  console.log('Server is running on port %d', port);
 });
